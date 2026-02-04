52f02937f958d0181fd88861e6d38ef9931a5f5a
Daniela <dggtn97@gmail.com>
2026-02-04 09:51:58 -0300

    subo atributos para la clase estudiante

--- demo/src/main/java/com/example/demo/entidad/Estudiante.java
+++ demo/src/main/java/com/example/demo/entidad/Estudiante.java
@@ -3,6 +3,8 @@ package com.example.demo.entidad;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.List;
+
 @Data
 @Entity
 public class Estudiante {
@@ -16,10 +18,19 @@ public class Estudiante {
 
     @Column
     private String nombre;
+    private String apellido;
+    private Institucion institucion;
+    private Calificacion calificacion;
+    private List <Calificacion> calificaciones;
+    private String nacionalidad;
 
-    public Estudiante(Long id, String nombre) {
+
+    public Estudiante(Long id, String nombre, String apellido, Institucion institucion, String nacionalidad) {
         this.id = id;
         this.nombre = nombre;
+        this.apellido = apellido;
+        this.institucion = institucion;
+        this.nacionalidad = nacionalidad;
     }
 }
 
--- demo/src/main/java/com/example/demo/entidad/dto/EstudianteDTO.java
+++ demo/src/main/java/com/example/demo/entidad/dto/EstudianteDTO.java
@@ -1,15 +1,22 @@
 package com.example.demo.entidad.dto;
 
+import com.example.demo.entidad.Calificacion;
+import com.example.demo.entidad.Institucion;
 import lombok.*;
+
+import java.util.List;
 
 
 @Data
 
 public class EstudianteDTO {
     private String nombre;
+    private String apellido;
+    private Institucion institucion;
+    private Calificacion calificacion;
+    private List<Calificacion> calificaciones;
+    private String nacionalidad;
 
-    public Object registrarCalificacion() {
-    }
 }
 
 
